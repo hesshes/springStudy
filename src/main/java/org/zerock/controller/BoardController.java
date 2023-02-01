ad72dd0da686e4fc34b7ce8374c3e4eb957dce67
hessh <hessh@taeu-dev-01>
2023-02-01 17:33:08 +0900

    20230201 dev1 // before modal

---
+++ src/main/java/org/zerock/controller/BoardController.java
@@ -0,0 +1,72 @@
+package org.zerock.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.zerock.domain.BoardVO;
+import org.zerock.service.BoardService;
+
+import lombok.Setter;
+import lombok.extern.log4j.Log4j;
+
+@Controller
+@Log4j
+@RequestMapping("/board/*")
+public class BoardController {
+
+	// spring 4.3 이상에서 단일 파라미터를 받는 생성자의 경우 필요한 파라미터를 자동으로 주입 가능
+	// 즉, lombok 사용시 AllArgsConstructor 어노테이션을 사용한 경우 autowired 생략 가능.
+
+	// @Autowired
+	@Setter(onMethod_ = @Autowired)
+	private BoardService boardService;
+
+	@GetMapping("/list")
+	public void list(Model model) {
+		log.info("list=============================");
+		model.addAttribute("list", boardService.getList());
+	}
+
+	@PostMapping("/register")
+	public String register(BoardVO board, RedirectAttributes rttr) {
+		log.info("register: " + board);
+		boardService.register(board);
+		rttr.addFlashAttribute("result", board.getBno());
+
+		// forward, redirect의 차이
+		// 간단하게 설명하면 요청 정보를 유지 vs 새로 요청 받음
+		// url 정보 유지 (forward) , 변화(redirect)
+		// 요청 객체 재사용 (forward), 새로운 객체(redirect)ㄴ
+		return "redirect:/board/list";
+	}
+
+	@GetMapping("/get")
+	public void get(@RequestParam("bno") Long bno, Model model) {
+		log.info("/get");
+		model.addAttribute("board", boardService.get(bno));
+	}
+
+	@PostMapping("//modify")
+	public String modify(BoardVO board, RedirectAttributes rttr) {
+		log.info("modify : " + board);
+		if (boardService.modify(board)) {
+			rttr.addFlashAttribute("result", "success");
+		}
+		return "redirect:/board/list";
+	}
+
+	@PostMapping("/remove")
+	public String remove(@RequestParam("bno") Long bno, RedirectAttributes rttr) {
+		log.info("remove.... : " + bno);
+		if (boardService.remove(bno)) {
+			rttr.addAttribute("result", "success");
+		}
+		return "redirect:/board/list";
+	}
+
+}
--- src/test/java/org/zerock/mapper/BoardMapperTests.java
+++ src/test/java/org/zerock/mapper/BoardMapperTests.java
@@ -25,65 +25,52 @@ public class BoardMapperTests {
 		boardMapper.getList().forEach(board -> log.info(board));
 	}
 
-	@Test
-	public void testInsert() {
-		BoardVO board = new BoardVO();
-		board.setTitle("새로 작성하는 글");
-		board.setContent("새로 작성하는 내용");
-		board.setWriter("newbie");
-
-		boardMapper.insert(board);
-
-		log.info("testInsert() ========================================");
-		log.info(board);
-
-	}
-
-	@Test
-	public void testInsertSelectKey() {
-		BoardVO board = new BoardVO();
-		board.setTitle("새로 작성하는 글 select Key");
-		board.setContent("새로 작성하는 내용 select Key");
-		board.setWriter("newbie");
-
-		boardMapper.insertSelectKey(board);
-
-		log.info("testInsertSelectKey() ========================================");
-		log.info(board);
-
-	}
-
-	@Test
-	public void testRead() {
-		log.info("testRead() ========================================");
-		BoardVO board = boardMapper.read(2L);
-
-		log.info(board);
-		System.out.println("testRead() : " + board.toString());
-
-	}
-
-	@Test
-	public void testDelete() {
-		log.info("testDelete() ========================================");
-		int count = boardMapper.delete(1L);
-
-		log.info("delete result : " + count);
-	}
-
-	@Test
-	public void testUpdate() {
-		log.info("testUpdate()================================");
-
-		BoardVO board = new BoardVO();
-		board.setContent("내용 수정");
-		board.setTitle("제목 수정");
-		board.setBno(2L);
-		board.setWriter("modifer");
-		
-		int result = boardMapper.update(board);
-
-		log.info("test update result : " + result);
-	}
+	/*
+	 * @Test public void testInsert() { BoardVO board = new BoardVO();
+	 * board.setTitle("새로 작성하는 글"); board.setContent("새로 작성하는 내용");
+	 * board.setWriter("newbie");
+	 * 
+	 * boardMapper.insert(board);
+	 * 
+	 * log.info("testInsert() ========================================");
+	 * log.info(board);
+	 * 
+	 * }
+	 * 
+	 * @Test public void testInsertSelectKey() { BoardVO board = new BoardVO();
+	 * board.setTitle("새로 작성하는 글 select Key");
+	 * board.setContent("새로 작성하는 내용 select Key"); board.setWriter("newbie");
+	 * 
+	 * boardMapper.insertSelectKey(board);
+	 * 
+	 * log.info("testInsertSelectKey() ========================================");
+	 * log.info(board);
+	 * 
+	 * }
+	 * 
+	 * @Test public void testRead() {
+	 * log.info("testRead() ========================================"); BoardVO
+	 * board = boardMapper.read(2L);
+	 * 
+	 * log.info(board); System.out.println("testRead() : " + board.toString());
+	 * 
+	 * }
+	 * 
+	 * @Test public void testDelete() {
+	 * log.info("testDelete() ========================================"); int count
+	 * = boardMapper.delete(1L);
+	 * 
+	 * log.info("delete result : " + count); }
+	 * 
+	 * @Test public void testUpdate() {
+	 * log.info("testUpdate()================================");
+	 * 
+	 * BoardVO board = new BoardVO(); board.setContent("내용 수정");
+	 * board.setTitle("제목 수정"); board.setBno(2L); board.setWriter("modifer");
+	 * 
+	 * int result = boardMapper.update(board);
+	 * 
+	 * log.info("test update result : " + result); }
+	 */
 
 }
